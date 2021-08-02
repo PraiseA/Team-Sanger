0e2538d28daa1cd4b3f02981897b91bec870b837
Marvellous O. Oyebanjo <88282876+Soullevram@users.noreply.github.com>
2021-08-02 23:29:16 +0100

    Add files via upload

---
+++ HackBio Task 1 (Marvellous).py
@@ -0,0 +1,18 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Defining my variables
+>>> 
+>>> #Name
+>>> Name = "Marvellous O. Oyebanjo"
+>>>
+>>> #Email Address
+>>> Email_Address = "marveloye@gmail.com"
+>>>
+>>> #Slack Username
+>>> Slack_Username = "@Marvellous"
+>>>
+>>> #Biostack
+>>> Biostack = "Genomics"
+>>>
+>>> #Printing each output on a new line
+>>> print("Name: {}\nEmail_Address: {}\nSlack_Username: {}\nBiostack: {}".format(Name, Email_Address, Slack_Username, Biostack))
