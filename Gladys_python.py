33051adb75b8e993fb0460b6920d5f5db9a8f708
glado718 <72727060+glado718@users.noreply.github.com>
2021-08-02 18:36:58 +0300

    Add files via upload

---
+++ Gladys_python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#Defining my variables
+
+#Name
+Name = "Gladys Rotich"
+
+#Email address
+Email_address = "gladysjerono86@gmail.com"
+
+#Slack username
+Slack_username = "@Gladys"
+
+#Preferred biostack
+Biostack = "Genomics, Data Science"
+
+#Printing each output on a new line
+print("Name: {}\nEmail_address: {}\nSlack_username: {}\nBiostack: {}".format(Name, Email_address , Slack_username , Biostack))
