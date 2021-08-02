4ee20e28b5ae50a8022e8ce36660cd713dd2e44f
Marvellous O. Oyebanjo <88282876+Soullevram@users.noreply.github.com>
2021-08-02 23:40:36 +0100

    Update HackBio Task 1 (Marvellous).py

--- HackBio Task 1 (Marvellous).py
+++ HackBio Task 1 (Marvellous).py
@@ -1,6 +1,7 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> #Defining my variables
+>>>#!/usr/bin/env python3
+>>>
+>>>
+>>>#Defining my variables
 >>> 
 >>> #Name
 >>> Name = "Marvellous O. Oyebanjo"
