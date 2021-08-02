965a42f61d2457faae1c7883e2a6e6bb21f42bd2
Marvellous O. Oyebanjo <88282876+Soullevram@users.noreply.github.com>
2021-08-02 23:47:54 +0100

    Add files via upload

---
+++ Marvellous (Task1).py
@@ -0,0 +1,28 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#!/usr/bin/env python3
+
+
+#Defining my variables
+
+
+#Name
+Name = "Marvellous O. Oyebanjo"
+
+
+#Email Address
+Email_Address = "marveloye@gmail.com"
+
+
+#Slack Username
+Slack_Username = "@Marvellous"
+
+
+#Biostack
+Biostack = "Genomics"
+
+
+#Printing each output on a new line
+print("Name: {}\nEmail_Address: {}\nSlack_Username: {}\nBiostack: {}".format(Name, Email_Address, Slack_Username, Biostack))
+
+
