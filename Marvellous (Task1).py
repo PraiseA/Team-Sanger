54b55871f3f035f8621bb9fc15c5abd7ec1d34e3
Marvellous O. Oyebanjo <88282876+Soullevram@users.noreply.github.com>
2021-08-02 23:49:02 +0100

    Update Marvellous (Task1).py

--- Marvellous (Task1).py
+++ Marvellous (Task1).py
@@ -1,5 +1,3 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 #!/usr/bin/env python3
 
 
@@ -24,5 +22,3 @@ Biostack = "Genomics"
 
 #Printing each output on a new line
 print("Name: {}\nEmail_Address: {}\nSlack_Username: {}\nBiostack: {}".format(Name, Email_Address, Slack_Username, Biostack))
-
-
