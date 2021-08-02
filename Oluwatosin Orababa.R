7b372b76dd732a50cfafa29225e8b353dc86a555
Oluwatosin Orababa <88286329+Orababa@users.noreply.github.com>
2021-08-02 21:53:51 +0100

    Add files via upload

---
+++ Oluwatosin Orababa.R
@@ -0,0 +1,20 @@
+#HackBio projects
+
+#task 1
+# Write a script that prints your name, your email, your slack username and your biostack.
+MyDetails = function(Name, Email, slack_username, biostack) {
+  details = paste(
+    "Hi! ",
+    "My name is ",
+    Name,
+    " with the email ",
+    Email,
+    " and slack username ",
+    slack_username,
+    ". I am interested in ",
+    biostack
+  )
+  print (details)
+  }
+MyDetails("Oluwatosin Orababa", "oluwatosinorababa@gmail.com", "@Oluwatosin", "microbial genomics, genomic epidemiology, and data science")
+
