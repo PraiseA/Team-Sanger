b27f18c0d92eecd65b25c9e7a277a51112cc84ef
nanjalaruth <ruthnanjala97@gmail.com>
2021-08-02 16:27:36 +0200

    Added a bash script for the given task

---
+++ Ruth.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+#Defining variables
+
+#Your name
+NAME="Ruth Nanjala"
+
+#my email address
+EMAIL="ruthnanjala97@gmail.com"
+
+#Slack Username
+SLACK_UNAME="@Nanje"
+
+#My preferred Biostack
+BIOSTACK="Genomics, Data Science"
+
+#Printing out the output Each on a new line
+printf "My name is: $NAME \nEmail address: $EMAIL \nSlack Username: $SLACK_UNAME \nBiostack: $BIOSTACK"
