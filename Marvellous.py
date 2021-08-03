beb77178d560ee19846ce25d32c9cbb565f21393
Marvellous O. Oyebanjo <88282876+Soullevram@users.noreply.github.com>
2021-08-03 21:46:05 +0100

    Add files via upload

---
+++ Marvellous.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+#Defining my variables
+
+
+#Name
+Name = "Marvellous O. Oyebanjo"
+
+
+#Email Address
+Email_Address = "marveloye@gmail.com"
+
+
+#Slack Username
+Slack_Username = "@Marvellous"
+
+
+#Biostack
+Biostack = "Genomics"
+
+
+#Printing each output on a new line
+print("{}\n{}\n{}\n{}".format(Name, Email_Address, Slack_Username, Biostack))
+
+
+
+
