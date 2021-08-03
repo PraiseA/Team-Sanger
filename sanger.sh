e42fa9acb140457c68afbc7135a043f12ff0fe78
Boluwatife Adewale <52474437+BBLinus@users.noreply.github.com>
2021-08-03 15:29:27 +0100

    bash script to create the csv

---
+++ sanger.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+
+#clone repo
+git clone https://github.com/PraiseA/Team-Sanger.git
+
+#grant execute permission for all files in the cloned folder
+cd Team-Sanger
+chmod a+x *
+cd ..
+
+#define header row for csv file
+header=NAME,EMAIL,USERNAME,BIOSTACK
+#write header row to csv file
+echo $header > sanger.csv
+
+#write every details written in python after converting to comma separated
+for i in Team-Sanger/*.py
+	do
+		./$i | sed -z 's/\n/,/g;s/,$/\n/' >> sanger.csv
+	done
+
+#write every details written in perl after converting to comma separated
+for i in Team-Sanger/*.pl
+        do
+                ./$i | sed -z 's/\n/,/g;s/,$/\n/' >> sanger.csv
+        done
+
+#write every details written in R after converting to comma separated
+for i in Team-Sanger/*.R
+        do
+                ./$i | sed -z 's/\n/,/g;s/,$/\n/' >> sanger.csv
+        done
+
+#write every details written in bash after converting to comma separated
+for i in Team-Sanger/Ruth.sh
+        do
+                ./$i | sed -z 's/\n/,/g;s/,$/\n/' >> sanger.csv
+        done
+
+#write every details written in C
+for i in Team-Sanger/*.c
+        do
+                ./$i | sed -z 's/\n/,/g;s/,$/\n/' >> sanger.csv
+        done
+
+#write every details written in C++
+for i in Team-Sanger/*.cpp
+        do
+                ./$i | sed -z 's/\n/,/g;s/,$/\n/' >> sanger.csv
+        done
+
+rm -rf Team-Sanger
