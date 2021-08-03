56e2061b3d4fad2fa1590c47e8d56a5d8e0a055a
Fredrick-kakembo <Kakembofredrick@gmail.com>
2021-08-04 00:40:52 +0300

    Added Fredrick's Javascript file

---
+++ Fredrick.js
@@ -0,0 +1,19 @@
+/*
+Javascript file
+Prints Name, email, Slack Username and my Biostack values
+separated by a comma on the same line
+
+To run the script, run
+node Fredrick.js
+*/
+
+// Define my name, email, username, and biostack variable
+
+var name = "Fredrick E. Kakembo";
+var email = "kakembofredrick@gmail.com";
+var s_uname = "@Fredrick";
+var biostack = "Genomics"
+
+// Print out the values
+
+console.log(name + "," + email + "," + s_uname + "," + biostack);
