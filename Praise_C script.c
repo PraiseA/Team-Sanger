211b12991db589a3f6cc1799d9e28ae53dbb4090
Achinebiri Praise <83349086+PraiseA@users.noreply.github.com>
2021-08-03 06:43:02 -0700

    Add files via upload

---
+++ Praise_C script.c
@@ -0,0 +1,14 @@
+// C script that prints my name, email address, slack username and biostack
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    printf("\n Name: Achinebiri Praise C");
+    printf("\n Email: richpraise36@gmail.com");
+    printf("\n Username: @Praise");
+    printf("\n Biostack: Drug Development");
+
+    return 0;
+}
