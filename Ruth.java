e22286a5f8bbe91e77ca4d223db65a8a56b48d6e
nanjalaruth <ruthnanjala97@gmail.com>
2021-08-03 19:53:43 +0300

    added the Java script for the task

---
+++ Ruth.java
@@ -0,0 +1,15 @@
+public class Ruth {
+  public static void main(String[] args) {
+    String Name = "Ruth Nanjala";
+    String Email = "ruthnanjala97@gmail.com";
+    String SlackUName = "@Nanje";
+    String Biostack = "Genomics & Data Science";
+    String full = Name + "," + Email + "," + SlackUName + "," + Biostack;
+    System.out.println(full);
+  }
+}
+
+
+
+
+
