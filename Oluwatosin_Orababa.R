6244e1a14a2aa2e1a121f077004da2120af73647
Oluwatosin Orababa <88286329+Orababa@users.noreply.github.com>
2021-08-04 21:37:39 +0100

    Updated

--- Oluwatosin_Orababa.R
+++ Oluwatosin_Orababa.R
@@ -1,5 +1,42 @@
-#Task 1
+#Defining my variables
 
-# My details
+Name="Oluwatosin Qawiyy ORABABA"
+Email="oluwatosinorababa@gmail.com"
+Slack_username="@Oluwatosin"
+Biostack="Genomics"
+Twitter_handle="@Horabz"
+hamming_distance= ""
 
-cat("Oluwatosin Qawiyy ORABABA \noluwatosinorababa@gmail.com \n@Oluwatosin \nGenomics")
+#Calculate hamming distance
+hamming_distance <- function(slack_username,Twitter_handle){
+  lenSlack = nchar(slack_username)
+  lenTwitter = nchar(Twitter_handle)
+  ham = 0
+  add = 0
+  if (lenSlack != lenTwitter) {
+    add = abs(lenSlack - lenTwitter)
+    i = 1
+    while (i <= lenSlack && lenSlack <= lenTwitter) {
+      if (substr(Slack_username,i,i) != substr(Twitter_handle,i,i)){
+        ham = ham + 1
+      }
+      i = i+1
+    }
+  }
+  else{
+    i = 1
+    while (i <= lenSlack) {
+      if (substr(Slack_username,i,i) != substr(Twitter_handle,i,i)){
+        ham = ham + 1
+      }
+      i = i+1
+    }
+  }
+  
+  return (ham+add)
+}
+hamming_distance = hamming_distance(Slack_username, Twitter_handle)
+
+
+#Print variables on same line with comma separation
+message(paste(Name, Email, Slack_username, Biostack, Twitter_handle, hamming_distance, sep=","))
