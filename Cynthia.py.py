357c6b2219023e2d33f5f00c980f9b17aef12166
Cynthia Orinda <87745988+orindacynthia21@users.noreply.github.com>
2021-08-04 17:05:11 +0300

    Add files via upload

---
+++ Cynthia.py.py
@@ -0,0 +1,19 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Defining my variables
+
+#my name
+>>> name="Cynthia.A.Orinda"
+
+#my email
+>>> email="orinda.cynthia@gmail.com"
+
+#my slack username
+>>> s_uname="@orinda"
+
+#my selected biostack
+>>> biostack='Genomics','Transcriptomics','Datascience'
+
+
+>>> personal_profile=name+","email+","s_uname+","biostack
+
