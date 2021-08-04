69f432f311989bca6e254f43b3659bbd59cfa038
Cynthia Orinda <87745988+orindacynthia21@users.noreply.github.com>
2021-08-04 18:29:42 +0300

    Add files via upload

---
+++ orindacyn.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+#Defining my variables
+
+#My name
+name="Cynthia Orinda"
+
+#my email address
+email="orinda.cynthia@gmail.com"
+
+#Slack Username
+s_uname="@Orinda"
+
+#My preferred Biostack
+biostack="Genomics&Transcriptomics"
+
+#print the output on a singleline but comma-separated
+print(name,email,s_uname,biostack, sep=',')
+
+
+
+
