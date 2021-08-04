fc349305735ba39a01c39ca3c287fb5178bf6107
Cynthia Orinda <87745988+orindacynthia21@users.noreply.github.com>
2021-08-04 11:09:17 +0300

    Add files via upload

---
+++ Cynthia.py.py
@@ -0,0 +1,22 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+>>> name=["Cynthia.A.Orinda"]
+>>> print(name)
+['Cynthia.A.Orinda']
+
+>>> email=["orinda.cynthia@gmail.com"]
+>>> print(email)
+['orinda.cynthia@gmail.com']
+
+>>> s_uname=["@orinda"]
+>>> print(s_uname)
+['@orinda']
+
+>>> biostack=['Genomics','Transcriptomics','Datascience']
+>>> print(biostack)
+['Genomics', 'Transcriptomics', 'Datascience']
+
+>>> personal_profile=['name','email','s_uname','biostack']
+>>> print(personal_profile)
+['name', 'email', 's_uname', 'biostack']
