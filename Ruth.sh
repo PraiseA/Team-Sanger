57544035a739ba807296c6bb175c24606da86316
Nanjala Ruth <55382239+nanjalaruth@users.noreply.github.com>
2021-08-04 18:44:31 +0300

    Update Ruth.sh

--- Ruth.sh
+++ Ruth.sh
@@ -21,27 +21,30 @@ TWITTER="@Ruthnanje"
 d=0
 
 #Check if slack name is longer than or equal to the twitter handle
-if len(s_uname) >= len(twitter):
-	#Iterate over the letters of the slack username and check letter per letter whether it mismatches 
-	for i in range(len(s_uname)):
-		#This is to catch instances where the shorter string falls out of range with the indexing
-		try:
-			#If the index letter for slack username and twitter are different, increament the value of d by one
-			if s_uname[i].lower() != twitter[i].lower():
-				d=d+1
-		except:
-			#When the shorter string falls out of range, still increament the value d by one for each instance
+if len(s_uname) >= len(twitter)
+then
+#Iterate over the letters of the slack username and check letter per letter whether it mismatches 
+	for i in range(len(s_uname))
+	#This is to catch instances where the shorter string falls out of range with the indexing
+	do
+	#If the index letter for slack username and twitter are different, increament the value of d by one
+		if s_uname[i].lower() != twitter[i].lower()
+		then
 			d=d+1
+		else
+			d=d+1
+	done
 
 #This portion will only run in case its the twitter name handle longer than the slack username. 
 #The rest of the code applies a similar approach as defined above.
-elif len(twitter) > len(s_uname):
-	for i in range(len(twitter)):
-                try:
-                        if twitter[i].lower() != s_uname[i].lower():
-                                d=d+1
-                except:
-                        d=d+1
-
+else len(twitter) > len(s_uname)
+	for i in range(len(twitter))
+	do
+        	if twitter[i].lower() != s_uname[i].lower()
+		then
+                	d=d+1
+                else
+                	d=d+1
+	done
 #Printing out the output Each on a new line
 printf "$NAME, $EMAIL, $SLACK_UNAME, $BIOSTACK, $TWITTER, d\n"
