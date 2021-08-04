91b4deb8d8a566423a92881f470ed044fe944df4
Achinebiri Praise <83349086+PraiseA@users.noreply.github.com>
2021-08-04 04:59:25 -0700

    Push C script

---
+++ Praise_C_Script.c
@@ -0,0 +1,36 @@
+// hamming distance calculation
+
+#include <stdio.h>
+#include <string.h>
+
+int hammingDist(char *str1, char *str2)
+{
+    int i = 0, count = 0;
+    while (str1[i] != '\0')
+    {
+        if (str1[i] != str2[i])
+            count++;
+        i++;
+    }
+    return count;
+}
+
+// calculating hamming distance between slack and twitter username
+// script printing my name, email, slack_username with @biostack, twitter_name and hamming_distance
+
+int main()
+{
+    char str1[] = "Praise";
+    char str2[] = "PR@I$E";
+    
+    strcpy(str1, "Praise");
+    strcpy(str2, "PR@I$E");
+    
+    printf("Achinebiri Praise C, ");
+    printf("richpraise36@gmail.com, ");
+    printf("Praise @Drug Development, ");
+    printf("PR@I$E, ");   
+    printf("and %o\n", hammingDist (str1, str2));
+    
+    return 0;
+}
