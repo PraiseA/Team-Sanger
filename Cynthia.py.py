2fa827d0eb3815b4a6d7de58d48d6f3cbc07a7b4
Cynthia Orinda <87745988+orindacynthia21@users.noreply.github.com>
2021-08-04 17:09:46 +0300

    Update Cynthia.py.py

--- Cynthia.py.py
+++ Cynthia.py.py
@@ -1,19 +1,18 @@
-Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 #Defining my variables
 
 #my name
->>> name="Cynthia.A.Orinda"
+name="Cynthia.A.Orinda"
 
 #my email
->>> email="orinda.cynthia@gmail.com"
+email="orinda.cynthia@gmail.com"
 
 #my slack username
->>> s_uname="@orinda"
+s_uname="@orinda"
 
 #my selected biostack
->>> biostack='Genomics','Transcriptomics','Datascience'
+biostack='Genomics','Transcriptomics','Datascience'
 
 
->>> personal_profile=name+","email+","s_uname+","biostack
+personal_profile=name+","email+","s_uname+","biostack
 
