7da0a12d1710778af044536933ce5d7a8a4bdf2e
Cynthia Orinda <87745988+orindacynthia21@users.noreply.github.com>
2021-08-04 17:28:25 +0300

    Add files via upload

---
+++ orindacyn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+#Defining my variables
+
+#My name
+name="Cynthia Orinda"
+
+#my email address
+email="orinda.cynthia@gmail.com"
+
+#Slack Username
+s_uname="@Orinda"
+
+#My preferred Biostack
+biostack="Genomics&Transcriptomics"
+
+
+#Printing out the output Each on a new line
+print("My name is: %s \nEmail: %s \nSlack Username: %s \nBiostack: %s\n" % (name,email,s_uname,biostack))
+
+
+#Print the output on the same line but tab-separated
+print("%s\t%s\t%s\t%s"% (name,email,s_uname,biostack))
+
+
