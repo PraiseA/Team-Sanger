3d109618753c96e4c9220584353d3d28a3478ae8
Marvellous O. Oyebanjo <88282876+Soullevram@users.noreply.github.com>
2021-08-04 12:22:53 +0100

    Update Marvellous.py

--- Marvellous.py
+++ Marvellous.py
@@ -22,15 +22,22 @@ Biostack = "Genomics"
 #Twitter Handle
 Twitter_Handle = "@MarvellousOye"
 
+# Python3 program to find hamming distance b/w two string
 
-print("{},{},{},{},{}".format(Name, Email_Address, Slack_Username, Biostack, Twitter_Handle))
+# Function to calculate Hamming distance
+def hammingDist (str1, str2):
+    i = 0
+    count = 0
 
+    while(i < len(str1)):
+        if(str1[i] != str2[i]):
+            count += 1
+        i += 1
+    return count
 
+# Driver code
+str1 = "MarvellousOye"
+str2 = "MarvellousOye"
 
-
-
-
-
-
-
-
+# function call
+print("{},{},{},{},{},{}".format(Name, Email_Address, Slack_Username, Biostack, Twitter_Handle, hammingDist (str1, str2)))
