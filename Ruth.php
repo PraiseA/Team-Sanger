acddd8b14a2ead15c870e0acaef1c2850af8c4b5
nanjalaruth <ruthnanjala97@gmail.com>
2021-08-05 21:04:46 +0300

    modified script to accomodate the hamming distance

--- Ruth.php
+++ Ruth.php
@@ -1,8 +1,45 @@
+<?php
+// php code
+// Assign variables
+?>
 
 <?php
 $name = "Ruth Nanjala";
 $email = "ruthnanjala97@gmail.com";
 $suname = "@Nanje";
 $Biostack = "Genomics & Data Science";
-echo "$name,$email,$suname,$Biostack\n";
+$twitter = "@Ruthnanje";
+
+// Calculate hamming distance
+
+$d=0;
+
+if (strlen($suname) == strlen($twitter)) {
+  // echo "Slack length is equal to twitter length!\n";
+  for ($i = 0; $i < strlen($suname); $i++) {
+    if ($suname[$i] != $twitter[$i]){
+      $d=$d+1;
+    }
+  }
+}elseif (strlen($suname) > strlen($twitter)) {
+  // echo "Slack length is greater than twitter length!\n";
+  $diff=strlen($suname)-strlen($twitter);
+  for ($i = 0; $i < strlen($twitter); $i++) {
+    if ($suname[$i] != $twitter[$i]){
+      $d=$d+1;
+    }
+  }
+  $d=$d+$diff;
+} else {
+  // echo "Twitter length is greater than slack length!\n";
+  $diff=strlen($twitter)-strlen($suname);
+  for ($i = 0; $i < strlen($suname); $i++) {
+    if ($suname[$i] != $twitter[$i]){
+      $d=$d+1;
+    }
+  }
+  $d=$d+$diff;
+}
+
+echo "$name,$email,$suname,$Biostack,$twitter,$d\n";
 ?>
