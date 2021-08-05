06e883c1707e8430ab097072d09776e99eb9d1c8
Marvellous O. Oyebanjo <88282876+Soullevram@users.noreply.github.com>
2021-08-05 14:54:20 +0100

    Update Marvellous.py

--- Marvellous.py
+++ Marvellous.py
@@ -22,26 +22,16 @@ Biostack = "Genomics"
 #Twitter Handle
 Twitter_Handle = "@MarvellousOye"
 
-#Calculate the Hamming distance, d, between the twitter handle and slack username
-
-#Initialise the value of Hamming distance, m
 m=0
 
-#Check if slack username is longer than or equal to twitter handle
 if len(Slack_Username) >= len(Twitter_Handle):
-    #Iterate over the letters of the slack username and check letter per letter whether it mismatches
     for i in range(len(Slack_Username)):
-        #This is to catch instances where the shorter string falls out of range with the indexing
         try:
-                #If the index letter for slack username and twitter are different, increase the value of d by one
-                if Slack_Username[i].lower() != Twitter_Handle[i].lower():
-                        m=m+1
+            if Slack_Username[i].lower() != Twitter_Handle[i].lower():
+                m=m+1
         except:
-                #When the shorter string falls out of range, still increase the value d by one for each instance
                 m=m+1
 
-#This portion will only run in case its twitter handle is longer than the slack username.
-#The rest of the code applies a similar approach as defined above.
 elif len(Twitter_Handle) > len(Slack_Username):
     for i in range(len(Twitter_Handle)):
             try:
@@ -50,7 +40,6 @@ elif len(Twitter_Handle) > len(Slack_Username):
             except:
                     m=m+1
 #print(d)
-
-#Print the output on the same line but comma-separated
+                    
+#print output
 print("{},{},{},{},{},{}".format(Name, Email_Address, Slack_Username, Biostack, Twitter_Handle, m))
-
