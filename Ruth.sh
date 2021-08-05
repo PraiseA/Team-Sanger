f3927cce6a314940f37dd1de7d2fd6df5f6ec7ef
nanjalaruth <ruthnanjala97@gmail.com>
2021-08-05 17:36:07 +0300

    modified the script to include the hamming distance

--- Ruth.sh
+++ Ruth.sh
@@ -7,28 +7,55 @@
 NAME="Ruth Nanjala"
 
 #my email address
-EMAIL="ruthnanjala97@gmail.com"
+EMAIL=ruthnanjala97@gmail.com
 
 #Slack Username
-SLACK_UNAME="@Nanje"
+SLACK_UNAME=@Nanje
 
 #My preferred Biostack
-BIOSTACK="Genomics"
+BIOSTACK=Genomics
 
 #Twitter
-TWITTER="@Ruthnanje"
+TWITTER=@Ruthnanje
 
+
+#Calculate the Hamming distance
 d=0
 
-if [ ${#SLACK_UNAME} -le ${#TWITTER} ]; then
-	for i in ${SLACK_UNAME}; do
-		if [ ${SLACK_UNAME,,}[i] != ${TWITTER,,}[i] ]; then
-			d=`expr $d + 1`
-		fi
-	
-	done
+#If slack and twitter have equal lengths
+if [ ${#SLACK_UNAME} -eq ${#TWITTER} ]
+then
+        for (( i=0; i<${#SLACK_UNAME}; i++ ))
+        do
+                if [ ${SLACK_UNAME:$i:1} != ${TWITTER:$i:1} ]; then
+                        d=$((d+1)); fi
+        done
+
+#If slack has a greater length than twitter
+elif [ ${#SLACK_UNAME} -gt ${#TWITTER} ]
+then
+        diff=$((${#SLACK_UNAME}-${#TWITTER}))
+        for (( i=0; i<${#TWITTER}; i++ ))
+        do
+                if [ ${SLACK_UNAME:$i:1} != ${TWITTER:$i:1} ]; then
+                        d=$((d+1)); fi
+        done
+
+        d=$((d+diff))
+
+#If twitter has a greater length than slack
+elif [ ${#TWITTER} -gt ${#SLACK_UNAME} ]
+then
+        diff=$((${#TWITTER}-${#SLACK_UNAME}))
+        for (( i=0; i<${#SLACK_UNAME}; i++ ))
+        do
+                if [ ${SLACK_UNAME:$i:1} != ${TWITTER:$i:1} ]; then
+                        d=$((d+1)); fi
+        done
+        d=$((d+diff))
 fi
-echo $d
+
+
 
 #Printing out the output
-#printf "$NAME,$EMAIL,$SLACK_UNAME,$BIOSTACK,$TWITTER,$d\n"
+printf "$NAME,$EMAIL,$SLACK_UNAME,$BIOSTACK,$TWITTER,$d\n"
