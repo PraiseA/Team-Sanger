da7252546309e5e955646155ebef90b45315ab59
Amarachukwu Chike-Onyeche <amaraonyichike@gmail.com>
2021-08-05 15:39:00 +0100

    Update Ruth.php

--- Ruth.php
+++ Ruth.php
@@ -1,7 +1,29 @@
+
 <?php
+// PHP program to find hamming distance b/w
+// two string
+ 
+// function to calculate
+// Hamming distance
+function hammingDist($str1, $str2)
+{
+    $i = 0; $count = 0;
+    while (isset($str1[$i]) != '')
+    {
+        if ($str1[$i] != $str2[$i])
+            $count++;
+        $i++;
+    }
+    return $count;
+}
+
+    $str1 = "@Nanje";
+    $str2 = "@Ruthnanje"
+
 $name = "Ruth Nanjala";
 $email = "ruthnanjala97@gmail.com";
 $suname = "@Nanje";
 $Biostack = "Genomics & Data Science";
+$Twitter
 echo "$name,$email,$suname,$Biostack\n";
 ?>
