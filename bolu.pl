0101d8994b86b9da22ce64cdea1f8a69ed3b7720
Boluwatife Adewale <52474437+BBLinus@users.noreply.github.com>
2021-08-05 02:57:17 +0100

    hamming update

--- bolu.pl
+++ bolu.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
 #Written in perl
 #Print each items separated by comma
-print "Boluwatife ADEWALE,boluadeleye03\@gmail.com,\@Bolu,Transcriptomics,\@bb_linux,8\n"
+print "Boluwatife ADEWALE,boluadeleye03\@gmail.com,\@Bolu,Transcriptomics,\@bb_linux,7\n"
