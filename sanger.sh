3a145b327e98ad2eb07bf82d7e2c197c07be1d65
Fredrick-kakembo <Kakembofredrick@gmail.com>
2021-08-06 22:44:07 +0300

    Finalised on the bash script

--- sanger.sh
+++ sanger.sh
@@ -1,87 +1,91 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-#clone repo
-#git clone https://github.com/PraiseA/Team-Sanger.git
+######################## Check if all the required tools are installed to run all the scripts #####################
+#The required programs are node, java, g++, gcc, perl, R, php							  #
+Program_exists() {												  #
+    command -v "$1" >/dev/null 2>&1 #Ignore the output								  #
+    if [[ $? -ne 0 ]]; then											  #
+        echo "This script requires $1 but it's not installed. Please install and try again.";  exit 1 ;		  #
+        fi  }													  #
+														  #
+for program in "javac" "java" "python" "g++" "gcc" "Rscript" "perl" "bash" "node" "php"; do			  #
+    Program_exists "${program}"; done										  #
+														  #
+###################################################################################################################
 
-#grant execute permission for all files in the cloned folder
-#cd Team-Sanger
-#chmod a+x *
-#cd ..
 
-#define header row for csv file
-header=NAME,EMAIL,USERNAME,BIOSTACK,TWITTER_HANDLE,HAMMING_DISTANCE
-#write header row to csv file
-echo $header > sanger.csv
+#Delete the Repository folder in case it exists
+[  -d Team-Sanger ] && rm -rf Team-Sanger
 
-#write every details written in python after converting to comma separated
-#for i in Team-Sanger/*.py
+#clone a new copy repo 
+git clone https://github.com/PraiseA/Team-Sanger.git
+cd Team-Sanger   #Change dir to Team scripts folder
 
-for py_file in *.py
+#Check if the outputfile exists, if yes delete and recreate empty one, or create new otherwise
+if [ -f sanger.out ]; then rm sanger.out; touch sanger.out; else  touch sanger.out; fi
+if [ -f ../sanger.csv ]; then rm ../sanger.csv;  fi
+
+for script in $(ls)
 do
-	python $py_file >> sanger.csv
+        extension=${script##*\.}
+
+        case $extension in
+
+	#Python scripts
+        py)
+                python $script >> sanger.out ;;
+
+        #R Scripts
+	R)
+                Rscript $script | sed 's/,$//' >> sanger.out ;;
+
+	# Perl scripts
+	pl)
+		perl $script >> sanger.out ;;
+
+	#For bash scripts
+	sh)
+                if [ $script != sanger.sh ]; then
+                	bash $script >> sanger.out
+		fi ;;
+
+	#For C scripts
+	c)
+		gcc $script
+		./a.out >> sanger.out
+	        rm a.out ;;
+ 	
+	#For C++ scripts
+	cpp)
+		g++ $script
+		./a.out >> sanger.out
+	        rm a.out ;;
+        
+	#For Java scripts
+	java)
+		javac $script
+		j_class=$(echo $script | sed 's/.java//')
+		java $j_class >> sanger.out
+		rm ${j_class}.class ;;
+	
+	# For PHP scripts
+	php)
+		php $script >> sanger.out ;;
+	
+	# For javascript scripts 
+	js)
+		node $script >>sanger.out ;;
+	esac
 done
 
-#write every details written in perl after converting to comma separated
-#for i in Team-Sanger/*.pl
-for pl_script in *pl
-do
-	perl $pl_script >> sanger.csv
-done
+cd ..  #Exit out of the folder
 
-#write every details written in R after converting to comma separated
-#for i in Team-Sanger/*.R
-
-for R_script in *.R
-do
-	Rscript $R_script >> sanger.csv
-done
-
-#write every details written in bash after converting to comma separated
-#for i in Team-Sanger/Ruth.sh
-#        do
-#                ./$i | sed -z 's/\n/,/g;s/,$/\n/' >> sanger.csv
-#        done
-
-#write every details written in C
-#for i in Team-Sanger/*.c
-for C_script in *.c
-do
-	gcc $C_script
-	./a.out >> sanger.csv
-	rm a.out
-done
-
-#write every details written in C++
-#for i in Team-Sanger/*.cpp
-for Cplus_script in *.cpp
-do
-	g++ $Cplus_script 
-	./a.out >> sanger.csv
-	rm a.out
-done
-
-#write every details written in java
-#for i in Team-Sanger/*.java
-for java_file in *.java
-do
-	javac $java_file 
-	j_class=$(echo $java_file | sed 's/.java//')
-	java $j_class >> sanger.csv
-	rm ${j_class}.class
-done
-
-#write every details written in php
-#for i in Team-Sanger/*.php
-for php_script in *.php
-do
-	php $php_script >> sanger.csv
-done
-
-#Execute Javascript
-for j_script in *.js
-do
-	node $j_script >>sanger.csv
-done
+sort Team-Sanger/sanger.out | uniq > sanger.csv  #Exclude duplicated entries
+rm Team-Sanger/sanger.out  # Delete the intermediate file
+cp sanger.csv Team-Sanger  #Keep a copy of the csv in the repo folder
 
 
 #rm -rf Team-Sanger
+
+
+echo -e "\n*******     THANK YOU FOR RUNNING TEAM SANGER's BASH SCRIPT.  BYE (^_^)     *******\n"
