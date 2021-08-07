5035f530348127a1a8edee82e2f47339742af42c
MallikaGakhar <80160010+MallikaGakhar@users.noreply.github.com>
2021-08-07 14:19:57 +0530

    Add files via upload

---
+++ mallika.py
@@ -0,0 +1,35 @@
+
+#Defining my variables
+
+#name
+name="Mallika"
+
+#email address
+email="malika.21313@gmail.com"
+
+#Slack username
+slack_uname="@Mallika"
+
+#Biostack
+biostack="Genomics"
+
+#twitter handle
+twitter="@mallika"
+
+#calculate hamming distance
+def hamming_distance(slack_uname,twitter):
+    count=0
+    for i in range(len(slack_uname)):
+      if slack_uname[i]!=twitter[i]:
+       count +=1
+    return count
+    if(len(slack_uname)== len(twitter)):
+          return count
+    else:
+       count=0
+       for i in range(len(slack_uname)):
+           if slack_uname[i]!=twitter[i]:
+               count +=1
+    return count
+
+print(name,email,slack_uname,biostack,twitter, hamming_distance(slack_uname,twitter), sep=',')
